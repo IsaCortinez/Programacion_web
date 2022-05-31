0e290c29a4eae4f3ff19ba4c8ea9cf10445a07df
Isabel Cortinez <89548294+IsaCortinez@users.noreply.github.com>
2022-05-30 22:57:10 -0500

    Sopa de letra con JavaScript

---
+++ SOPADELETRAS.js
@@ -0,0 +1,289 @@
+var a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,
+b10,c6,c11, d6,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,
+e12,e13,e14,e15,e16,e17,e18,e19,e20,e21,e22,
+i1,i2;
+
+
+//palabra Header color rojo
+function comprobarheader1(){
+a1 = document.getElementById('header1').style.backgroundColor="red";
+}
+
+function comprobarheader2(){
+a2 = document.getElementById('header2').style.backgroundColor="red";
+}
+function comprobarheader3(){
+a3 = document.getElementById('header3').style.backgroundColor="red";
+}
+function comprobarheader4(){
+a4 = document.getElementById('header4').style.backgroundColor="red";
+}
+
+function comprobarheader5(){
+a5 = document.getElementById('header5').style.backgroundColor="red";
+}
+
+function comprobarheader6(){
+a6 = document.getElementById('header6').style.backgroundColor="red";
+}
+
+
+//palabra Section color blue
+function comprobarsection1(){
+a7 = document.getElementById('section1').style.backgroundColor="blue";
+}
+function comprobarsection2(){
+a8 = document.getElementById('section2').style.backgroundColor="blue";
+}
+function comprobarsection3(){
+a9 = document.getElementById('section3').style.backgroundColor="blue";
+}
+function comprobarsection4(){
+a10 = document.getElementById('section4').style.backgroundColor="blue";
+}
+
+function comprobarsection5(){
+b10 = document.getElementById('section5').style.backgroundColor="blue";
+}
+
+function comprobarsection6(){
+c11 = document.getElementById('section6').style.backgroundColor="blue";
+}
+
+function comprobarsection7(){
+c6 = document.getElementById('section7').style.backgroundColor="blue";
+}
+
+
+//palabra Tcp color brown
+function comprobartcp1(){
+d6 = document.getElementById('tcp1').style.backgroundColor="orange";
+}
+
+function comprobartcp2(){
+e6 = document.getElementById('tcp2').style.backgroundColor="orange";
+}
+
+
+//palabra Figma color brown
+function comprobarfigma1(){
+f6 = document.getElementById('figma1').style.backgroundColor="brown";
+}
+function comprobarfigma2(){
+g6 = document.getElementById('figma2').style.backgroundColor="brown";
+}
+
+function comprobarfigma3(){
+h6 = document.getElementById('figma3').style.backgroundColor="brown";
+}
+function comprobarfigma4(){
+i6 = document.getElementById('figma4').style.backgroundColor="brown";
+}
+
+
+//palabra head color yellow
+function comprobarhead1(){
+j6 = document.getElementById('head1').style.backgroundColor="#DABFFF";
+}
+function comprobarhead2(){
+e1 = document.getElementById('head2').style.backgroundColor="#DABFFF";
+}
+
+
+//palabra html color purple
+function comprobarhtml1(){
+e2 = document.getElementById('html1').style.backgroundColor="purple";
+}
+function comprobarhtml2(){
+e3 = document.getElementById('html2').style.backgroundColor="purple";
+}
+function comprobarhtml3(){
+e4 = document.getElementById('html3').style.backgroundColor="purple";
+}
+
+
+//palabra  div color yellow
+function comprobardiv1(){
+e5 = document.getElementById('div1').style.backgroundColor="#7FDEFF";
+}
+function comprobardiv2(){
+e7 = document.getElementById('div2').style.backgroundColor="#7FDEFF";
+}
+
+
+//palabra  css color white
+function comprobarcss1(){
+e8 = document.getElementById('css1').style.backgroundColor="green";
+}
+function comprobarcss2(){
+e9 = document.getElementById('css2').style.backgroundColor="green";
+}
+function comprobarcss3(){
+e10 = document.getElementById('css3').style.backgroundColor="green";
+}
+
+
+//palabra dns color black
+function comprobardns1(){
+    e11 = document.getElementById('dns1').style.backgroundColor="black";
+}
+function comprobardns2(){
+    e12 = document.getElementById('dns2').style.backgroundColor="black";
+}
+
+
+
+//palabra main color black
+function comprobarmain1(){
+    e13 = document.getElementById('main1').style.backgroundColor="black";
+}
+function comprobarmain2(){
+    e14 = document.getElementById('main2').style.backgroundColor="black";
+}
+function comprobarmain3(){
+    e15 = document.getElementById('main3').style.backgroundColor="black";
+}
+
+
+//palabra ul color red
+function comprobarul1(){
+    e16 = document.getElementById('ul1').style.backgroundColor="black";
+}
+function comprobarul2(){
+    e17 = document.getElementById('ul2').style.backgroundColor="black";
+}
+
+
+
+//palabra ul color red
+function comprobarol1(){
+    e18 = document.getElementById('ol1').style.backgroundColor="black";
+}
+
+
+//palabra api color balck
+function comprobarapi1(){
+    e19 = document.getElementById('api1').style.backgroundColor="black";
+}
+function comprobarapi2(){
+    e20 = document.getElementById('api2').style.backgroundColor="black";
+}
+
+
+
+
+//palabra span color yellow
+function comprobarspan1(){
+    e21 = document.getElementById('span1').style.backgroundColor="#DABFFF";
+}
+function comprobarspan2(){
+    e22 = document.getElementById('span2').style.backgroundColor="#DABFFF";
+}
+
+
+
+//palabra class color blue
+function comprobarclass1(){
+    i3 = document.getElementById('class1').style.backgroundColor="blue";
+}
+function comprobarclass2(){
+    i3 = document.getElementById('class2').style.backgroundColor="blue";
+}
+function comprobarclass3(){
+    i3 = document.getElementById('class3').style.backgroundColor="blue";
+}
+function comprobarclass4(){
+    i3 = document.getElementById('class4').style.backgroundColor="blue";
+}
+function comprobarclass5(){
+    i3 = document.getElementById('class5').style.backgroundColor="blue";
+}
+
+
+//palabra body color orange
+function comprobarbody1(){
+    i3 = document.getElementById('body1').style.backgroundColor="orange";
+}
+function comprobarbody2(){
+    i3 = document.getElementById('body2').style.backgroundColor="orange";
+}
+function comprobarbody3(){
+    i3 = document.getElementById('body3').style.backgroundColor="orange";
+}
+function comprobarbody4(){
+    i3 = document.getElementById('body4').style.backgroundColor="orange";
+}
+
+
+//palabra articule color purple
+function comprobararticule1(){
+    i3 = document.getElementById('articule1').style.backgroundColor="purple";
+}
+function comprobararticule2(){
+    i3 = document.getElementById('articule2').style.backgroundColor="purple";
+}
+function comprobararticule3(){
+    i3 = document.getElementById('articule3').style.backgroundColor="purple";
+}
+function comprobararticule4(){
+    i3 = document.getElementById('articule4').style.backgroundColor="purple";
+}
+function comprobararticule5(){
+    i3 = document.getElementById('articule5').style.backgroundColor="purple";
+}
+function comprobararticule6(){
+    i3 = document.getElementById('articule6').style.backgroundColor="purple";
+}
+function comprobararticule7(){
+    i3 = document.getElementById('articule7').style.backgroundColor="purple";
+}
+
+
+//palabra footer color orange
+function comprobarfooter1(){
+    i3 = document.getElementById('footer1').style.backgroundColor="orange";
+}
+function comprobarfooter2(){
+    i3 = document.getElementById('footer2').style.backgroundColor="orange";
+}
+function comprobarfooter3(){
+    i3 = document.getElementById('footer3').style.backgroundColor="orange";
+}
+function comprobarfooter4(){
+    i3 = document.getElementById('footer4').style.backgroundColor="orange";
+}
+function comprobarfooter5(){
+    i3 = document.getElementById('footer5').style.backgroundColor="orange";
+}
+function comprobarfooter6(){
+    i3 = document.getElementById('footer6').style.backgroundColor="orange";
+}
+
+
+//palabra title color white
+function comprobartitle1(){
+    i3 = document.getElementById('title1').style.backgroundColor="blue";
+}
+function comprobartitle2(){
+    i3 = document.getElementById('title2').style.backgroundColor="blue";
+}
+function comprobartitle3(){
+    i3 = document.getElementById('title3').style.backgroundColor="blue";
+}
+function comprobartitle4(){
+    i3 = document.getElementById('title4').style.backgroundColor="blue";
+}
+function comprobartitle5(){
+    i3 = document.getElementById('title5').style.backgroundColor="blue";
+}
+
+
+
+//palabra img color orange
+function comprobarimg1(){
+    i3 = document.getElementById('img1').style.backgroundColor="green";
+}
+function comprobarimg2(){
+    i3 = document.getElementById('img2').style.backgroundColor="green";
+}
+
